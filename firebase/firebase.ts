54505b5c19bd94bbe6b86109787e6d163d430fc0
prabhat <prabhatshukla0849@gmal.com>
2024-08-12 18:16:52 +0530

    an interactive discord like app

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -2,16 +2,21 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import firebase from "firebase/app";
+import "firebase/auth";
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBb1FjQVs7MPdPm6N_onLJjH4Ek5K0JJXI",
+  authDomain: "prabhat-9250c.firebaseapp.com",
+  databaseURL: "https://prabhat-9250c-default-rtdb.firebaseio.com",
+  projectId: "prabhat-9250c",
+  storageBucket: "prabhat-9250c.appspot.com",
+  messagingSenderId: "1537208931",
+  appId: "1:1537208931:web:277f84b2e8c0a6a3b47e56",
+  measurementId: "G-T9ER7RKSHL"
 };
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
